1b63c19bc016bb7d277d1c3d85e7f840f91a8a39
HariAr2 <ha7014@srmist.edu.in>
2023-12-14 23:18:02 +0530

    Added project

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React,{useState, useEffect} from "react";
+import "./components/style.css";
+import { AnimeList } from "./components/AnimeList";
+import { AnimeInfo } from "./components/AnimeInfo";
+function App() {
+  const [animeData,setAnimeData] = useState(); 
+  const [searchAnime,setSearchAnime] = useState('Naruto');
+  const [animeInfo,setAnimeInfo] = useState();
+
+  const getData= async()=>{
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${searchAnime}&limit=20`);
+    const resData = await res.json();
+    setAnimeData(resData.data);
+    console.log(resData);
+
+  }  
+
+  useEffect(()=>{
+    getData();
+  },[searchAnime])
+
+  return (
+    <>
+    <div className="header">
+      <h1>MyAnimeVault</h1>
+      <div className="search-box">
+        <input type="search" placeholder="anime name..."
+          onChange={(e) => setSearchAnime(e.target.value)} />
+      </div>
+    </div>
+    <div className="container">
+        <div className="anime-info">
+          {animeInfo && <AnimeInfo/>}
+        </div>
+        <div className="anime-row">
+          <h2 className="text-heading">Anime</h2>
+          <div className="row">
+            <AnimeList animelist={animeData}
+            setAnimeInfo={setAnimeInfo}/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/AnimeList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export const AnimeList = ({ animelist,setAnimeInfo }) => {
+  console.log(animelist);
+  
+  return (
+    <>
+      {animelist ? (
+        animelist.map((anime, index) => {
+          return (
+            <div className="card" key={index}>
+              {anime.images && anime.images.jpg && anime.images.jpg.large_image_url ? (
+                <img src={anime.images.jpg.large_image_url} alt="animeImg" />
+              ) : (
+                <img src="placeholder_image_url" alt="Placeholder" /> // Placeholder image or error handling
+              )}
+              <div className="anime-info">
+                <h4>{anime.title}</h4>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <div>Loading ... ... ...</div>
+      )}
+    </>
+  );
+};
