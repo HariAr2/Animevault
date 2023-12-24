0bac09a120d5e5c72f8a0d195aada31aa1256295
HariAr2 <ha7014@srmist.edu.in>
2023-12-24 22:13:20 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./components/style.css";
 import { AnimeList } from "./components/AnimeList";
 import { AnimeInfo } from "./components/AnimeInfo";
 
-
 function App() {
+  // State variables
+  const [animeData, setAnimeData] = useState(); // Holds fetched anime data
+  const [searchAnime, setSearchAnime] = useState('Naruto'); // Stores the searched anime
+  const [animeInfo, setAnimeInfo] = useState(); // Stores selected anime information
 
-  
-  const [animeData,setAnimeData] = useState(); 
-  const [searchAnime,setSearchAnime] = useState('Naruto'); 
-  const [animeInfo,setAnimeInfo] = useState(); 
-
-  const getData= async()=>{
+  // Function to fetch anime data based on search query
+  const getData = async () => {
     const res = await fetch(`https://api.jikan.moe/v4/anime?q=${searchAnime}&limit=20`);
     const resData = await res.json();
     setAnimeData(resData.data);
-    console.log(resData); 
+    console.log(resData);
+  };
 
-  }   
- 
-  useEffect(()=>{ 
+  // Fetch data on initial load and when searchAnime changes
+  useEffect(() => {
     getData();
-  },[searchAnime])
- 
-  return ( 
+  }, [searchAnime]);
+
+  return (
     <>
-    <div className="header">
-      <h1>MyAnimeVault</h1>
-      <div className="search-box">
-        <input type="search" placeholder="anime name..." 
-          onChange={(e) => setSearchAnime(e.target.value)} />
+      {/* Header section */}
+      <div className="header">
+        <h1>MyAnimeVault</h1>
+        {/* Search box */}
+        <div className="search-box">
+          <input
+            type="search"
+            placeholder="anime name..."
+            onChange={(e) => setSearchAnime(e.target.value)}
+          />
+        </div>
       </div>
-    </div>
-    <div className="container">
+      {/* Main content */}
+      <div className="container">
+        {/* Display selected anime information */}
         <div className="anime-info">
-          {animeInfo && <AnimeInfo/>}
-        </div> 
+          {animeInfo && <AnimeInfo />}
+        </div>
+        {/* Display list of anime */}
         <div className="anime-row">
           <h2 className="text-heading">Anime</h2>
           <div className="row">
-            <AnimeList animelist={animeData}
-            setAnimeInfo={setAnimeInfo}/> 
+            {/* Pass fetched anime data and setter for animeInfo */}
+            <AnimeList animelist={animeData} setAnimeInfo={setAnimeInfo} />
           </div>
         </div>
-      </div> 
-    </>  
+      </div>
+    </>
   );
 }
-
 
 export default App;
