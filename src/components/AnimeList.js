9996f5d1d0018456e18716e3518ca1240c0e33d7
HariAr2 <ha7014@srmist.edu.in>
2023-12-25 22:39:39 +0530

    added comments

--- src/components/AnimeList.js
+++ src/components/AnimeList.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
-
-export const AnimeList = ({ animelist,setAnimeInfo }) => {
+export const AnimeList = ({ animelist, setAnimeInfo }) => {
   console.log(animelist);
-  
+
   return (
     <>
       {animelist ? (
         animelist.map((anime, index) => {
           return (
             <div className="card" key={index}>
-              {anime.images && anime.images.jpg && anime.images.jpg.large_image_url ? (
+              {anime.images &&
+              anime.images.jpg &&
+              anime.images.jpg.large_image_url ? (
                 <img src={anime.images.jpg.large_image_url} alt="animeImg" />
               ) : (
                 <img src="placeholder_image_url" alt="Placeholder" /> // Placeholder image or error handling
