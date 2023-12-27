8170b65e71e384c61381c5503e4eaa6a0d31108a
Hari Aravindh <127916422+HariAr2@users.noreply.github.com>
2023-12-27 22:57:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./components/style.css";
 import { AnimeList } from "./components/AnimeList";
-import { AnimeInfo } from "./components/AnimeInfo";
+import { AnimeInfo } from "./components/AnimeInfo"; 
 
 function App() {
   // State variables
