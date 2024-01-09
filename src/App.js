eff972d67a303ed9e5c0b4fec071a3a3a4a2b588
HariAr2 <ha7014@srmist.edu.in>
2024-01-09 23:06:55 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
           <div className="row">
             {/* Pass fetched anime data and setter for animeInfo */}
             <AnimeList animelist={animeData} setAnimeInfo={setAnimeInfo} />
-            
+
           </div>
         </div>
       </div>
